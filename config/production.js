3aed0324d11e845fe8f8a200d03d68b5cadc13f9
maguohua <1264889788@qq.com>
2017-04-23 14:32:05 +0800

    test 阿里云

--- config/production.js
+++ config/production.js
@@ -2,5 +2,5 @@
 
 export default {
 	port: 80,
-	url: 'mongodb://d762000ca46b4ada9a372db528e48f47:786336dc346e4d3787c7cc96a4ab3312@mongo.duapp.com:8908/sxouaLdjCuZUZSRDwuGn'
+	url: 'mongodb://localhost:27017/elm'
 }
