b966d2eaac0d116dd5ff2850e9b19c2ace1716a0
maguohua1 <1264889788@qq.com>
2018-11-25 22:02:19 +0800

    add ignore

--- app.js
+++ app.js
@@ -17,14 +17,10 @@ const app = express();
 app.all('*', (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", req.headers.origin || req.headers.referer || '*');
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE");
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
 	res.header("X-Powered-By", 'Express');
-	if (req.method == 'OPTIONS') {
-  	res.sendStatus(200);
-	} else {
-    next();
-	}
+  next();
 });
 
 // app.use(Statistic.apiRecord)
