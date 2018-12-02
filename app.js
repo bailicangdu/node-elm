e84dc6fc0750d6ba88273e6f652ab2759fb405ad
maguohua1 <1264889788@qq.com>
2018-12-02 16:20:13 +0800

    set allow-origin

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ import chalk from 'chalk';
 const app = express();
 
 app.all('*', (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.origin || req.headers.referer || '*');
+  const { origin, Origin, referer, Referer } = req.headers;
+  const allowOrigin = origin || Origin || referer || Referer || '*';
+	res.header("Access-Control-Allow-Origin", allowOrigin);
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
